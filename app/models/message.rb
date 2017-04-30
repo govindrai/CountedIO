75d790fd9fb90ae513e82a0f2f80befb13269a3c
Daniel Welsh <danielwelsh@borealproductions.com>
2017-04-30 13:00:27 -0700

    Create registration checker and change wit back to message() method

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,9 +4,30 @@ require 'wit'
 class Message < ApplicationRecord
   belongs_to :user
 
+
+
   def do_easy_shit
-    puts send_message_to_wit
+    wit_response = send_message_to_wit
+    check_for_registration(wit_response)
   end
+
+
+
+  def check_for_registration(wit_response)
+    p '*' * 100
+    puts wit_response
+    p '*' * 100
+    if wit_response["action"] = "saveIntent"
+      p '*' * 100
+      puts 'yes! please save me'
+      p '*' * 100
+    end
+  end
+
+
+
+
+
 
   def send_message_to_wit
     configure_wit_client
@@ -15,44 +36,44 @@ class Message < ApplicationRecord
     #User messages
     p "*" * 50
     p "Response 1"
-    rsp = @client.converse('my-user-session-42', self.body, {})
-    puts("Yay, got Wit.ai response: #{rsp}")
+    rsp = @client.message(self.body)
+    # puts("Yay, got Wit.ai response: #{rsp}")
 
-    p "*" * 50
-    p "Response 2"
-    #Sever Applies context
-    rsp = @client.converse('my-user-session-42', {intent: "register"})
-    puts("Yay, got Wit.ai response: #{rsp}")
+    # p "*" * 50
+    # p "Response 2"
+    # #Sever Applies context
+    # rsp = @client.converse('my-user-session-42', {intent: "register"})
+    # puts("Yay, got Wit.ai response: #{rsp}")
 
-    p "*" * 50
-    p "Response 3"
-    #user message
-    rsp = @client.converse('my-user-session-42', 'Govind Rai', {intent: "register"})
-    puts("Yay, got Wit.ai response: #{rsp}")
+    # p "*" * 50
+    # p "Response 3"
+    # #user message
+    # rsp = @client.converse('my-user-session-42', 'Govind Rai', {intent: "register"})
+    # puts("Yay, got Wit.ai response: #{rsp}")
 
-    p "*" * 50
-    p "Response 4"
-    #server applies context
-    rsp = @client.converse('my-user-session-42', {intent: "register", name: "Govind Rai"})
-    puts("Yay, got Wit.ai response: #{rsp}")
+    # p "*" * 50
+    # p "Response 4"
+    # #server applies context
+    # rsp = @client.converse('my-user-session-42', {intent: "register", name: "Govind Rai"})
+    # puts("Yay, got Wit.ai response: #{rsp}")
 
-    p "*" * 50
-    p "Response 5"
-    #user message
-    rsp = @client.converse('my-user-session-42', {intent: "register", name: "Govind Rai"})
-    puts("Yay, got Wit.ai response: #{rsp}")
+    # p "*" * 50
+    # p "Response 5"
+    # #user message
+    # rsp = @client.converse('my-user-session-42', {intent: "register", name: "Govind Rai"})
+    # puts("Yay, got Wit.ai response: #{rsp}")
 
-    p "*" * 50
-    p "Response 6"
-    #user message
-    rsp = @client.converse('my-user-session-42', "20", {intent: "register", name: "Govind Rai"})
-    puts("Yay, got Wit.ai response: #{rsp}")
+    # p "*" * 50
+    # p "Response 6"
+    # #user message
+    # rsp = @client.converse('my-user-session-42', "20", {intent: "register", name: "Govind Rai"})
+    # puts("Yay, got Wit.ai response: #{rsp}")
 
-    p "*" * 50
-    p "Response 7"
-    #user message
-    rsp = @client.converse('my-user-session-42', {intent: "register", name: "Govind Rai", age: "20"})
-    puts("Yay, got Wit.ai response: #{rsp}")
+    # p "*" * 50
+    # p "Response 7"
+    # #user message
+    # rsp = @client.converse('my-user-session-42', {intent: "register", name: "Govind Rai", age: "20"})
+    # puts("Yay, got Wit.ai response: #{rsp}")
   end
 
   def send_test_message_to_govind
@@ -67,11 +88,9 @@ class Message < ApplicationRecord
   end
 
   def configure_twilio_client
-    p Rails.application.secrets
     Twilio.configure do |config|
       config.account_sid = Rails.application.secrets.twilio_account_sid
       config.auth_token = Rails.application.secrets.twilio_auth_token
-      p Rails.application.secrets.twilio_account_sid
     end
     @twilio_phone_number = '+19253504172'
     @client = Twilio::REST::Client.new
