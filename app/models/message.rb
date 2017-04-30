5b6a44c295b1258e9add792e934ac5d823e4d689
Govind Rai <raigovind93@gmail.com>
2017-04-30 01:40:36 -0700

    Add intial test action for twilio integration

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,28 @@
+require 'twilio-ruby'
+
 class Message < ApplicationRecord
   belongs_to :user
+
+  private
+
+  def send_test_message_to_govind
+    configure_twilio_client
+    @client.messages.create(
+      from: @twilio_phone_number,
+      to: '+19257779777',
+      body: 'Hey there!',
+      media_url: 'http://example.com/smileyface.jpg'
+    )
+  end
+
+  def configure_twilio_client
+    Twilio.configure do |config|
+      config.account_sid = Rails.application.secrets.some_api_key.twilio_account_sid
+      config.auth_token = Rails.application.secrets.some_api_key.twilio_auth_token
+    end
+    @twilio_phone_number = +19253504172
+    @client = Twilio::REST::Client.new
+  end
+
+
 end
