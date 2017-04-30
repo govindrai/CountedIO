4595909b46181de9ef6ebac441e5fc09f950a1a1
Govind Rai <raigovind93@gmail.com>
2017-04-29 23:53:14 -0700

    Add index on users table

--- db/migrate/20170430055341_create_users.rb
+++ db/migrate/20170430055341_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :phone_number, null:false
+      t.string :phone_number, null:false, index:true
       t.integer :age, null:false
       t.integer :weight_pounds, null:false
       t.integer :height_inches, null:false
