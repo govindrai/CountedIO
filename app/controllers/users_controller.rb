a42927add4a91be0b19d1b71cbdff39a34b655a9
Daniel Welsh <danielwelsh@borealproductions.com>
2017-05-01 12:14:13 -0700

    Add seed data to test User show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     if @user.randomized_profile_url == params[:random]
       @authorized = true
     end
+    @meals = Meal.where(user: @user)
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+dan = User.create(name: "Daniel Welsh", phone_number: "+17802636496", age: 20, weight_pounds: 170, target_weight_pounds: 180, sex: "male", height_inches: 74)
+
+Meal.create(user: dan, food_name: "Chicken", calories: 123, quantity: 2, meal_type: "breakfast")
+
+Meal.create(user: dan, food_name: "Celery", calories: 5, quantity: 5, meal_type: "breakfast")
+
+Meal.create(user: dan, food_name: "Waffles", calories: 450, quantity: 2, meal_type: "breakfast")
+
+Meal.create(user: dan, food_name: "Ice Cream", calories: 200, quantity: 1, meal_type: "breakfast")
+
+Meal.create(user: dan, food_name: "Cereal", calories: 400, quantity: 1, meal_type: "lunch")
+
+Meal.create(user: dan, food_name: "Sandwich", calories: 300, quantity: 1, meal_type: "lunch")
+
+Meal.create(user: dan, food_name: "ham", calories: 150, quantity: 3, meal_type: "dinner")
+
+Meal.create(user: dan, food_name: "Grilled Chicken Breast", calories: 300, quantity: 1, meal_type: "dinner")
+
+Meal.create(user: dan, food_name: "Cake", calories: 300, quantity: 1, meal_type: "dinner")
