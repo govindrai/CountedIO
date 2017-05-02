1592d8b842e680554bfbaea35bcc41b31ee419ec
Daniel Welsh <danielwelsh@borealproductions.com>
2017-05-02 16:50:10 -0700

    Refactor User model with  public/private methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,31 +4,9 @@ class User < ApplicationRecord
 
   before_save :generate_randomized_profile_url, :set_maintainance_calories, :set_weight_direction
 
-  def set_weight_direction
-    if self.target_weight_pounds < self.weight_pounds
-      self.weight_direction = "Weight Gain"
-    elsif self.target_weight_pounds > self.weight_pounds
-      self.weight_direction = "Weight Loss"
-    else
-      self.weight_direction = "Maintain Weight"
-    end
-  end
-
-  def generate_randomized_profile_url
-    random = %w(a b c d e f g h i j k l m n o p q r s t u v w y z A B C D E F G H I J K L M N O P Q R S T U V W Y Z 1 2 3 4 5 6 7 8 9)
-
-    random_url = ''
-    10.times {|time| random_url += random.sample }
-    self.randomized_profile_url = random_url
-  end
-
   def generate_link_to_profile
-    random = %w(a b c d e f g h i j k l m n o p q r s t u v w y z A B C D E F G H I J K L M N O P Q R S T U V W Y Z 1 2 3 4 5 6 7 8 9)
     base_url = "https://vildeio.herokuapp.com/profile/#{self.id}?random="
-    generate_randomized_profile_url
-    random_url = self.randomized_profile_url
-    message = "Here is your profile:  "
-    message + base_url + random_url
+    message = "Here is your profile:  " + base_url + generate_randomized_profile_url
   end
 
   def get_calories_summary
@@ -41,22 +19,41 @@ class User < ApplicationRecord
     message = "You have consumed #{calories_consumed} calories today. You may only consume #{remaining_calories} more to meet your daily goal."
   end
 
+  private
+
+  def generate_randomized_profile_url
+    random = %w(a b c d e f g h i j k l m n o p q r s t u v w y z A B C D E F G H I J K L M N O P Q R S T U V W Y Z 1 2 3 4 5 6 7 8 9)
+    random_url = ''
+    10.times {|time| random_url += random.sample }
+    self.randomized_profile_url = random_url
+  end
+
+  def set_weight_direction
+    if self.target_weight_pounds < self.weight_pounds
+      self.weight_direction = "Weight Gain"
+    elsif self.target_weight_pounds > self.weight_pounds
+      self.weight_direction = "Weight Loss"
+    else
+      self.weight_direction = "Maintain Weight"
+    end
+  end
+
   def set_maintainance_calories
     activity_level = {"Sitting all day": 1.2, "Seated work, no exercise": 1.3, "Seated work, light exercise": 1.4, "Moderately physical, no exercise": 1.5, "Moderately physical work, light exercise": 1.6, "Moderately physical work, heavy exercise": 1.7, "Heavy work/ heavy exercise": 1.8, "Above average physical activity": 2}
 
     if self.sex == "male" || self.sex =="Male"
-      (1.4 * suggested_male_calories).round()
+      (1.4 * bmr_formula_male).round()
     else
-      (1.4 * suggested_female_calories).round()
+      (1.4 * bmr_formula_female).round()
     end
   end
 
-  def suggested_male_calories
+  def bmr_formula_male
     bmr = 10 * (self.weight_pounds * 0.453592) + 6.25 * (self.height_inches * 2.54) - 5 * self.age + 5
   end
 
-  def suggested_female_calories
-    bmr = 10 * (seld.weight_pounds * 0.453592) + 6.25 * (self.height_inches * 2.54) - 5 * self.age - 161
+  def bmr_formula_female
+    bmr = 10 * (self.weight_pounds * 0.453592) + 6.25 * (self.height_inches * 2.54) - 5 * self.age - 161
   end
 
 
