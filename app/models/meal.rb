ce61f42c131b73fa6649162ec899dd2d90409bc8
Daniel Welsh <danielwelsh@borealproductions.com>
2017-05-02 15:34:49 -0700

    Add method to collect get_day_meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,25 @@
 class Meal < ApplicationRecord
   belongs_to :user
+
+
+  def self.get_day_meals(user, date_obj)
+    Meal.where("user_id = ? AND created_at >= ?", user.id, (date_obj.to_time + 7.hours).to_datetime)
+  end
+
+  def self.get_day_breakfast)user, date_obj)
+
+  end
+
+  def self.get_day_lunch(user, date_obj)
+
+  end
+
+  def self.get_day_dinner(user, date_obj)
+
+  end
+
+  def self.get_day_snack(user.date_obj)
+
+  end
+
 end
