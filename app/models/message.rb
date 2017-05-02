bc9f4b2ce7e5d261040d72ffa74ee2429ab58784
Govind Rai <raigovind93@gmail.com>
2017-05-02 02:54:52 -0700

    Add print statment to debug

--- app/models/message.rb
+++ app/models/message.rb
@@ -38,6 +38,7 @@ class Message < ApplicationRecord
     message_fail = ""
     foods_array.each do |food_obj|
       if food_obj[:food] == "User Defined Calories"
+        puts food_obj
         Meal.create({
           user: @user,
           food_name: food_obj[:food],
