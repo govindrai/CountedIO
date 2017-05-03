25c2610b558bca79f06cda0f602a3fac8b9a310e
Govind Rai <raigovind93@gmail.com>
2017-05-03 16:36:51 -0700

    Fix seed data for meal types

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+
+
 gov = User.create(name:"govind", age:24, weight_pounds:190, target_weight_pounds:175, height_inches:70, sex:"male", phone_number: "+19257779777")
 
 dan = User.create(name: "Daniel Welsh", phone_number: "+17802636496", age: 20, weight_pounds: 170, target_weight_pounds: 180, sex: "male", height_inches: 74)
 
-Meal.create(user: dan, food_name: "Chicken", calories: 123, quantity: 2, meal_type: "breakfast", original_description: "lala")
+Meal.create(user: dan, food_name: "Chicken", calories: 123, quantity: 2, meal_type: "Breakfast", original_description: "lala")
 
-Meal.create(user: dan, food_name: "Celery", calories: 5, quantity: 5, meal_type: "breakfast", original_description: "lala")
+Meal.create(user: dan, food_name: "Celery", calories: 5, quantity: 5, meal_type: "Breakfast", original_description: "lala")
 
-Meal.create(user: dan, food_name: "Waffles", calories: 450, quantity: 2, meal_type: "breakfast", original_description: "lala")
+Meal.create(user: dan, food_name: "Waffles", calories: 450, quantity: 2, meal_type: "Breakfast", original_description: "lala")
 
-Meal.create(user: dan, food_name: "Ice Cream", calories: 200, quantity: 1, meal_type: "breakfast", original_description: "lala")
+Meal.create(user: dan, food_name: "Ice Cream", calories: 200, quantity: 1, meal_type: "Breakfast", original_description: "lala")
 
-Meal.create(user: dan, food_name: "Cereal", calories: 400, quantity: 1, meal_type: "lunch", original_description: "lala")
+Meal.create(user: dan, food_name: "Cereal", calories: 400, quantity: 1, meal_type: "Lunch", original_description: "lala")
 
-Meal.create(user: dan, food_name: "Sandwich", calories: 300, quantity: 1, meal_type: "lunch", original_description: "lala")
+Meal.create(user: dan, food_name: "Sandwich", calories: 300, quantity: 1, meal_type: "Lunch", original_description: "lala")
 
-Meal.create(user: dan, food_name: "ham", calories: 150, quantity: 3, meal_type: "dinner", original_description: "lala")
+Meal.create(user: dan, food_name: "ham", calories: 150, quantity: 3, meal_type: "Dinner", original_description: "lala")
 
-Meal.create(user: dan, food_name: "Grilled Chicken Breast", calories: 300, quantity: 1, meal_type: "dinner", original_description: "lala")
+Meal.create(user: dan, food_name: "Grilled Chicken Breast", calories: 300, quantity: 1, meal_type: "Dinner", original_description: "lala")
 
-<<<<<<< HEAD
-Meal.create(user: dan, food_name: "Cake", calories: 300, quantity: 1, meal_type: "dinner", original_description: "lala")
-=======
-Meal.create(user: dan, food_name: "Cake", calories: 300, quantity: 1, meal_type: "dinner", original_description: "lala")
+Meal.create(user: dan, food_name: "Cake", calories: 300, quantity: 1, meal_type: "Dinner", original_description: "lala")
+
+Meal.create(user: dan, food_name: "Cake", calories: 500, quantity: 1, meal_type: "Dinner", original_description: "lala")
 
 
 
@@ -43,9 +45,9 @@ Meal.create(user: dan, food_name: "Cake", calories: 300, quantity: 1, meal_type:
 
 # Meal.create(user: govind, food_name: "Ice Cream", calories: 200, quantity: 1, meal_type: "breakfast")
 
-# Meal.create(user: govind, food_name: "Cereal", calories: 400, quantity: 1, meal_type: "lunch")
+# Meal.create(user: govind, food_name: "Cereal", calories: 400, quantity: 1, meal_type: "Lunch")
 
-# Meal.create(user: govind, food_name: "Sandwich", calories: 300, quantity: 1, meal_type: "lunch")
+# Meal.create(user: govind, food_name: "Sandwich", calories: 300, quantity: 1, meal_type: "Lunch")
 
 # Meal.create(user: govind, food_name: "ham", calories: 150, quantity: 3, meal_type: "dinner")
 
@@ -60,4 +62,3 @@ Meal.create(user: dan, food_name: "Cake", calories: 300, quantity: 1, meal_type:
 # Meal.create(user: govind, food_name: "dank", calories: 3020, quantity: 1, meal_type: "dinner")
 
 # Meal.create(user: govind, food_name: "Cake", calories: 300, quantity: 1, meal_type: "dinner")
->>>>>>> a5590160610606a82e33f998cdec8e85699acad4
