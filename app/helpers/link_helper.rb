c1d65cb038abf00ce0331f18696319f25fe45a0d
Daniel Welsh <danielwelsh@borealproductions.com>
2017-05-03 09:48:46 -0700

    Add previous_day and refactor code into user_show_base to be DRY

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,10 +1,18 @@
 module LinkHelper
 
-  def self.next_day(date, user)
+  def self.user_show_base(user)
     base = Rails.application.routes.url_helpers.profile_path(user.id)
     random = "&random=" + user.randomized_profile_url
-    date = "&date=" + (date + 1.days).strftime("%F")
+    link = base + random
   end
 
-  def
+  def self.next_day(date, user)
+    date = "&date=" + (date + 1.days).strftime("%F")
+    link = LinkHelper.user_show_base(user) + date
+  end
+
+  def self.previous_day(date, user)
+    date = "&date=" + (date - 1.days).strftime("%F")
+    link = LinkHelper.user_show_base(user) + date
+  end
 end
