e94f3b93cae8a708825958c2f903b45816132629
Govind Rai <raigovind93@gmail.com>
2017-05-05 12:31:45 -0700

    Compile images and css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -243,7 +243,7 @@ var getDayMeals = function () {
 
 var getDayMealsOnToggle = function () {
   var baseUrl = $(document)[0].URL.split("?")
-  var url = baseUrl[0] + "get_day_meals" + "?" + baseUrl[1]
+  var url = baseUrl[0] + "/get_day_meals" + "?" + baseUrl[1]
   var date = $(this).parent().parent().find('.date')
   var direction = $(this).attr('class')
   var data = "?date=" + dateLabel.text().substring(0,11) + "&direction=" + direction
