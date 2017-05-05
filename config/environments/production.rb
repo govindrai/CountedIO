edf2dcabc4e3d45e691a9cf930011f0910f59535
Govind Rai <raigovind93@gmail.com>
2017-05-05 01:21:28 -0700

    config.assets.initialize_on_precompile = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.initialize_on_precompile = false
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
