a8215ded2fd24874fc6c8ac36337a0c7b1ac5b3b
Govind Rai <raigovind93@gmail.com>
2017-05-05 02:31:11 -0700

    Fix bug where attribute was not saving in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -94,7 +94,8 @@ class User < ApplicationRecord
     random = %w(a b c d e f g h i j k l m n o p q r s t u v w y z A B C D E F G H I J K L M N O P Q R S T U V W Y Z 1 2 3 4 5 6 7 8 9)
     random_url = ''
     10.times {|time| random_url += random.sample }
-    self.randomized_profile_url = random_url
+    self.update(randomized_profile_url: random_url)
+    self.randomized_profile_url
   end
 
   private
